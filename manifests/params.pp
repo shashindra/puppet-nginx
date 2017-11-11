124f88bec5e213cc923e385bf493b4f6e144273c
root <root@shashiudawa1.mylabserver.com>
2017-11-11 03:09:53 +0000

    fixed package name nginx

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class nginx::params{
  $ensure          = 'present'
- $package_name    = 'nginx'
+ $package_name    = 'pe-nginx'
  $config_dir      = '/etc/nginx'
  $config_ensure   = 'file'
  $config_mode     = '0664'
